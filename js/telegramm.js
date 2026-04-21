566dc34f8b5442eb7b4f234b52a61dc3fd80d92d
Tbhate <dmitriybut83@gmail.com>
2026-04-21 14:41:01 +0200

    form

--- js/telegramm.js
+++ js/telegramm.js
@@ -1,43 +1,43 @@
-const form = document.getElementById("telegramForm");
-const statusText = document.getElementById("status");
+// const form = document.getElementById("telegramForm");
+// const statusText = document.getElementById("status");
 
-form.addEventListener("submit", async function (e) {
-  e.preventDefault();
+// form.addEventListener("submit", async function (e) {
+//   e.preventDefault();
 
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
+//   const name = document.getElementById("name").value;
+//   const email = document.getElementById("email").value;
 
-  const TOKEN = "8715834935:AAE1aE6JfiDaSYZJ5iPtNTT8f98E58Skvcg";
-  const CHAT_ID = "-5183115862";
+//   const TOKEN = "";
+//   const CHAT_ID = "";
 
-  const message = `
-📩 New message from site
+//   const message = `
+// 📩 New message from site
 
-👤 Name: ${name}
-📧 Email: ${email}
-`;
+// 👤 Name: ${name}
+// 📧 Email: ${email}
+// `;
 
-  const url = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+//   const url = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
 
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        chat_id: CHAT_ID,
-        text: message,
-      }),
-    });
+//   try {
+//     const response = await fetch(url, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         chat_id: CHAT_ID,
+//         text: message,
+//       }),
+//     });
 
-    if (response.ok) {
-      statusText.textContent = "Message sent!";
-      form.reset();
-    } else {
-      statusText.textContent = "Error!";
-    }
-  } catch (error) {
-    statusText.textContent = "Connection error!";
-  }
-});
+//     if (response.ok) {
+//       statusText.textContent = "Message sent!";
+//       form.reset();
+//     } else {
+//       statusText.textContent = "Error!";
+//     }
+//   } catch (error) {
+//     statusText.textContent = "Connection error!";
+//   }
+// });
